b3f504d2be5fb86e4597d894207b7c0eba8d988d
feitian124 <feitian124@gmail.com>
2021-07-31 20:46:42 +0800

    check function line works

--- pkg/convert/function.go
+++ pkg/convert/function.go
@@ -6,36 +6,36 @@ import (
 	"strings"
 )
 
-type AssertResult struct {
+type FunctionResult struct {
 	match bool
+	function string
 	caller string
-	actual string
-	checker string
-	expect string
+	name string
+	rest string
 }
 
-func Equals(line string) (*AssertResult, error) {
-	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*Equals),(?P<expect>.+)\)`
+func Function(line string) (*FunctionResult, error) {
+	p := `(?P<function>\s*func\s*)(?P<caller>\(.*\)\s*)(?P<name>Test.*)(?P<rest>\(.*\)\s*\{\s*)`
 	r := regexp.MustCompile(p)
 	match := r.FindStringSubmatch(line)
 	groupNames := r.SubexpNames()
 	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
-	result := &AssertResult{}
+	result := &FunctionResult{}
 	if len(match) == len(groupNames) {
 		// 转换为map
 		for i, name := range groupNames {
 			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
+				if name == "function" {
+					result.function = strings.TrimSpace(match[i])
+				}
 				if name == "caller" {
 					result.caller = strings.TrimSpace(match[i])
 				}
-				if name == "actual" {
-					result.actual = strings.TrimSpace(match[i])
+				if name == "name" {
+					result.name = strings.TrimSpace(match[i])
 				}
-				if name == "checker" {
-					result.checker = strings.TrimSpace(match[i])
-				}
-				if name == "expect" {
-					result.expect = strings.TrimSpace(match[i])
+				if name == "rest" {
+					result.rest = strings.TrimSpace(match[i])
 				}
 				result.match = true
 			}
@@ -44,3 +44,4 @@ func Equals(line string) (*AssertResult, error) {
 
 	return result, nil
 }
+
