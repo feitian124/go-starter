b117b5754e39dc959cf6d46dd20ac2385dfc7735
feitian124 <feitian124@gmail.com>
2021-07-31 20:43:10 +0800

    check function line works

--- pkg/convert/assert.go
+++ pkg/convert/assert.go
@@ -7,14 +7,15 @@ import (
 )
 
 type AssertResult struct {
+	match bool
 	caller string
 	actual string
 	checker string
 	expect string
 }
 
-func assert(line string) (*AssertResult, error) {
-	p := `(?P<caller>\w+)\.Assert\((?P<actual>\s*[A-Za-z0-9._()]+),(?P<checker>\s*\w+),(?P<expect>\s*[A-Za-z0-9._()]+)\)`
+func Equals(line string) (*AssertResult, error) {
+	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*Equals),(?P<expect>.+)\)`
 	r := regexp.MustCompile(p)
 	match := r.FindStringSubmatch(line)
 	groupNames := r.SubexpNames()
@@ -36,6 +37,46 @@ func assert(line string) (*AssertResult, error) {
 				if name == "expect" {
 					result.expect = strings.TrimSpace(match[i])
 				}
+				result.match = true
+			}
+		}
+	}
+
+	return result, nil
+}
+
+type FunctionResult struct {
+	match bool
+	function string
+	caller string
+	name string
+	rest string
+}
+
+func Function(line string) (*FunctionResult, error) {
+	p := `(?P<function>\s*func\s*)(?P<caller>\(.*\)\s*)(?P<name>Test.*)(?P<rest>\(.*\)\s*\{\s*)`
+	r := regexp.MustCompile(p)
+	match := r.FindStringSubmatch(line)
+	groupNames := r.SubexpNames()
+	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
+	result := &FunctionResult{}
+	if len(match) == len(groupNames) {
+		// 转换为map
+		for i, name := range groupNames {
+			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
+				if name == "function" {
+					result.function = strings.TrimSpace(match[i])
+				}
+				if name == "caller" {
+					result.caller = strings.TrimSpace(match[i])
+				}
+				if name == "name" {
+					result.name = strings.TrimSpace(match[i])
+				}
+				if name == "rest" {
+					result.rest = strings.TrimSpace(match[i])
+				}
+				result.match = true
 			}
 		}
 	}
--- pkg/convert/assert_test.go
+++ pkg/convert/assert_test.go
@@ -5,21 +5,52 @@ import (
 	"testing"
 )
 
-func Test_assert(t *testing.T) {
+func TestEquals(t *testing.T) {
 	table := []struct {
 		line   string
 		result *AssertResult
 	}{
 		{"c.Assert(stmtEvictedElement.beginTime, Equals, now)",
-			&AssertResult{caller: "c", actual: "stmtEvictedElement.beginTime", checker: "Equals", expect: "now"},
+			&AssertResult{match: true, caller: "c", actual: "stmtEvictedElement.beginTime", checker: "Equals", expect: "now"},
+		},
+		{"c.Assert(stmtEvictedElement.beginTime, Greater, now)",
+			&AssertResult{match: false, caller: "", actual: "", checker: "", expect: ""},
 		},
 		{"  c.Assert(stmtEvictedElement.beginTime(), Equals, now1) ",
-			&AssertResult{caller: "c", actual: "stmtEvictedElement.beginTime()", checker: "Equals", expect: "now1"},
+			&AssertResult{match: true, caller: "c", actual: "stmtEvictedElement.beginTime()", checker: "Equals", expect: "now1"},
+		},
+		{`c.Assert(getAllEvicted(ssbde), Equals, "{begin: 5, end: 6, count: 1}, {begin: 1, end: 2, count: 2}")`,
+			&AssertResult{match: true, caller: "c", actual: "getAllEvicted(ssbde)", checker: "Equals", expect: `"{begin: 5, end: 6, count: 1}, {begin: 1, end: 2, count: 2}"`},
 		},
 	}
 
 	for _, v := range table {
-		x, err := assert(v.line)
+		x, err := Equals(v.line)
+		require.NoError(t, err)
+		require.Equal(t, v.result, x)
+	}
+}
+
+func TestFunction(t *testing.T) {
+	table := []struct {
+		line   string
+		result *FunctionResult
+	}{
+		{"  func (s *testStmtSummarySuite) TestSimpleStmtSummaryByDigestEvicted(c *C) {  ",
+			&FunctionResult{match: true, function: "func", caller: "(s *testStmtSummarySuite)", name: "TestSimpleStmtSummaryByDigestEvicted", rest: "(c *C) {"},
+		},
+		{
+			"func (s *testStmtSummarySuite) TestNewStmtSummaryByDigestEvictedElement(c *C) {",
+			&FunctionResult{match: true, function: "func", caller: "(s *testStmtSummarySuite)", name: "TestNewStmtSummaryByDigestEvictedElement", rest: "(c *C) {"},
+		},
+		{
+			"func NewStmtSummaryByDigestEvictedElement(c *C) {",
+			&FunctionResult{match: false, function: "", caller: "", name: "", rest: ""},
+		},
+	}
+
+	for _, v := range table {
+		x, err := Function(v.line)
 		require.NoError(t, err)
 		require.Equal(t, v.result, x)
 	}
