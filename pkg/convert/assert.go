197061e54e4bfb9253519eb17e0a81d3040d009b
feitian124 <feitian124@gmail.com>
2021-07-31 21:53:33 +0800

    重构, 抽象出一个公共方法

--- pkg/convert/assert.go
+++ pkg/convert/assert.go
@@ -16,149 +16,27 @@ type AssertResult struct {
 
 func Equals(line string) (*AssertResult, error) {
 	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*Equals),(?P<expect>.+)\)`
-	r := regexp.MustCompile(p)
-	match := r.FindStringSubmatch(line)
-	groupNames := r.SubexpNames()
-	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
-	result := &AssertResult{}
-	if len(match) == len(groupNames) {
-		// 转换为map
-		for i, name := range groupNames {
-			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
-				if name == "caller" {
-					result.caller = strings.TrimSpace(match[i])
-				}
-				if name == "actual" {
-					result.actual = strings.TrimSpace(match[i])
-				}
-				if name == "checker" {
-					result.checker = strings.TrimSpace(match[i])
-				}
-				if name == "expect" {
-					result.expect = strings.TrimSpace(match[i])
-				}
-				result.match = true
-			}
-		}
-	}
-
-	return result, nil
+	return Assert(line, p)
 }
 
 func DeepEquals(line string) (*AssertResult, error) {
 	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*DeepEquals),(?P<expect>.+)\)`
-	r := regexp.MustCompile(p)
-	match := r.FindStringSubmatch(line)
-	groupNames := r.SubexpNames()
-	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
-	result := &AssertResult{}
-	if len(match) == len(groupNames) {
-		// 转换为map
-		for i, name := range groupNames {
-			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
-				if name == "caller" {
-					result.caller = strings.TrimSpace(match[i])
-				}
-				if name == "actual" {
-					result.actual = strings.TrimSpace(match[i])
-				}
-				if name == "checker" {
-					result.checker = strings.TrimSpace(match[i])
-				}
-				if name == "expect" {
-					result.expect = strings.TrimSpace(match[i])
-				}
-				result.match = true
-			}
-		}
-	}
-
-	return result, nil
+	return Assert(line, p)
 }
-
 
 func IsNil(line string) (*AssertResult, error) {
 	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*IsNil)\)`
-	r := regexp.MustCompile(p)
-	match := r.FindStringSubmatch(line)
-	groupNames := r.SubexpNames()
-	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
-	result := &AssertResult{}
-	if len(match) == len(groupNames) {
-		// 转换为map
-		for i, name := range groupNames {
-			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
-				if name == "caller" {
-					result.caller = strings.TrimSpace(match[i])
-				}
-				if name == "actual" {
-					result.actual = strings.TrimSpace(match[i])
-				}
-				if name == "checker" {
-					result.checker = strings.TrimSpace(match[i])
-				}
-				result.match = true
-			}
-		}
-	}
-
-	return result, nil
+	return Assert(line, p)
 }
 
 func NotNil(line string) (*AssertResult, error) {
 	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*NotNil)\)`
-	r := regexp.MustCompile(p)
-	match := r.FindStringSubmatch(line)
-	groupNames := r.SubexpNames()
-	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
-	result := &AssertResult{}
-	if len(match) == len(groupNames) {
-		// 转换为map
-		for i, name := range groupNames {
-			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
-				if name == "caller" {
-					result.caller = strings.TrimSpace(match[i])
-				}
-				if name == "actual" {
-					result.actual = strings.TrimSpace(match[i])
-				}
-				if name == "checker" {
-					result.checker = strings.TrimSpace(match[i])
-				}
-				result.match = true
-			}
-		}
-	}
-
-	return result, nil
+	return Assert(line, p)
 }
 
 func IsTrue(line string) (*AssertResult, error) {
 	p := `(?P<caller>\w+)\.Assert\((?P<actual>.+),(?P<checker>\s*IsTrue)\)`
-	r := regexp.MustCompile(p)
-	match := r.FindStringSubmatch(line)
-	groupNames := r.SubexpNames()
-	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
-	result := &AssertResult{}
-	if len(match) == len(groupNames) {
-		// 转换为map
-		for i, name := range groupNames {
-			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
-				if name == "caller" {
-					result.caller = strings.TrimSpace(match[i])
-				}
-				if name == "actual" {
-					result.actual = strings.TrimSpace(match[i])
-				}
-				if name == "checker" {
-					result.checker = strings.TrimSpace(match[i])
-				}
-				result.match = true
-			}
-		}
-	}
-
-	return result, nil
+	return Assert(line, p)
 }
 
 func IsFalse(line string) (*AssertResult, error) {
@@ -175,4 +53,34 @@ func Greater(line string) (*AssertResult, error) {
 
 func GreaterEqual(line string) (*AssertResult, error) {
 	return nil, nil
+}
+
+func Assert(line string, p string) (*AssertResult, error) {
+	r := regexp.MustCompile(p)
+	match := r.FindStringSubmatch(line)
+	groupNames := r.SubexpNames()
+	fmt.Printf("%v, %v, %d, %d\n", match, groupNames, len(match), len(groupNames))
+	result := &AssertResult{}
+	if len(match) == len(groupNames) {
+		// 转换为map
+		for i, name := range groupNames {
+			if i != 0 && name != "" { // 第一个分组为空（也就是整个匹配）
+				if name == "caller" {
+					result.caller = strings.TrimSpace(match[i])
+				}
+				if name == "actual" {
+					result.actual = strings.TrimSpace(match[i])
+				}
+				if name == "checker" {
+					result.checker = strings.TrimSpace(match[i])
+				}
+				if name == "expect" {
+					result.expect = strings.TrimSpace(match[i])
+				}
+				result.match = true
+			}
+		}
+	}
+
+	return result, nil
 }
