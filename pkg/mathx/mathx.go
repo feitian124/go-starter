3eca74468a3afad8c7828974e6f6867f869c70ed
feitian124 <feitian124@gmail.com>
2021-08-17 22:17:28 +0800

    add RoundInt

--- pkg/mathx/mathx.go
+++ pkg/mathx/mathx.go
@@ -8,13 +8,17 @@ import "math"
 // A value with a fractional part less than .5 is rounded down to the next integer if positive or up to the next
 // integer if negative. (In other words, it is rounded toward zero.)
 func RoundInt(i int64, dec int) int64 {
+	// is itself when dec >= 0
 	if dec >= 0 {
 		return i
 	}
+	// The integer part of a fraction
 	shift := math.Pow10(dec)
-	r := math.Round(float64(i) * shift)
+	intPart := math.Round(float64(i) * shift)
+	r := int64(intPart)
+	// the zero part
 	for i := 1; i <= -dec; i++ {
 		r *= 10
 	}
-	return int64(r)
+	return r
 }
